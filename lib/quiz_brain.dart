1d85a6dd56969c2dd6e3d067d3cd7db0416d0e13
Volodymyr Zhyhaylo <volodymyr.zhyhaylo@gmail.com>
2020-09-14 03:14:36 +0300

    Third

---
+++ lib/quiz_brain.dart
@@ -0,0 +1,11 @@
+import 'package:quizzler/question.dart';
+
+import './question.dart';
+
+class QuizBrain {
+  List<Question> questionBank = [
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+  ];
+}
